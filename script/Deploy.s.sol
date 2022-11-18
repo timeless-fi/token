64b9a123f2c7ab60b96b87d2cacff5a343dc1df3
ZeframLou <zeframlou@gmail.com>
2022-11-18 13:07:48 -0800

    feat: better versioning in deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,30 +1,25 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.13;
 
-import {CREATE3Factory} from "create3-factory/CREATE3Factory.sol";
-
-import "forge-std/Script.sol";
-
+import {CREATE3Script} from "./base/CREATE3Script.sol";
 import {TimelessToken} from "../src/TimelessToken.sol";
 
-contract DeployScript is Script {
+contract DeployScript is CREATE3Script("1.0.0") {
     function run() external returns (TimelessToken timelessToken) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
-        address deployer = vm.addr(deployerPrivateKey);
-        CREATE3Factory create3 = CREATE3Factory(0x9fBB3DF7C40Da2e5A0dE984fFE2CCB7C47cd0ABf);
 
         string memory name = vm.envString("NAME");
         string memory symbol = vm.envString("SYMBOL");
         address owner = vm.envAddress("OWNER");
 
         // assumes the options token was deployed with the same deployer account
-        address optionsToken = create3.getDeployed(deployer, keccak256("OptionsToken-v1.0.0"));
+        address optionsToken = getCreate3Contract("OptionsToken");
 
         vm.startBroadcast(deployerPrivateKey);
 
         timelessToken = TimelessToken(
             create3.deploy(
-                keccak256("TimelessToken-v1.0.0"),
+                getCreate3ContractSalt("TimelessToken"),
                 bytes.concat(type(TimelessToken).creationCode, abi.encode(name, symbol, owner, optionsToken))
             )
         );
